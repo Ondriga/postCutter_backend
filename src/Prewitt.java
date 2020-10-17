af00db43d41c8c361bb39f5a0fa785328926a000
Patrik Ondriga <xondri08@vutbr.cz>
2020-10-18 00:47:09 +0200

    Prepare class for prewitt operator mathod

---
+++ src/Prewitt.java
@@ -0,0 +1,10 @@
+import javax.swing.ImageIcon;
+
+public class Prewitt implements EdgeDetector {
+
+    @Override
+    public ImageIcon highlightEdge(String sourceImage) {
+        return null;
+    }
+
+}
