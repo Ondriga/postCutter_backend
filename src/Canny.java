d6667242010665228ebc2c7bddea90d0fb2fad25
Patrik Ondriga <xondri08@vutbr.cz>
2020-10-25 21:12:33 +0100

    Create class for canny edge detection

---
+++ src/Canny.java
@@ -0,0 +1,41 @@
+/*
+ * Source code for the backend of Bachelor thesis.
+ * Canny class
+ * 
+ * (C) Patrik Ondriga (xondri08)
+ */
+
+import java.awt.image.BufferedImage;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+/**
+ * Representing canny edge detector.
+ */
+public class Canny extends EdgeDetector {
+
+    /// Constant for low threshold of canny method
+    private static final int THRESHOLD1 = 40;
+    /// Constant for high threshold of canny method
+    private static final int THRESHOLD2 = 40*3;
+
+    /**
+     * Constructor
+     * @param methodName method name
+     */
+    public Canny(String methodName) {
+        super(methodName);
+    }
+
+    @Override
+    public BufferedImage highlightEdge(String sourceImage) {
+        Mat srcMat = getGrayScale(sourceImage);
+        Mat destMat = new Mat(srcMat.rows(), srcMat.cols(), srcMat.type());
+
+        Imgproc.Canny(srcMat, destMat, THRESHOLD1, THRESHOLD2);
+
+        return mat2BufferedImage(destMat);
+    }
+    
+}
