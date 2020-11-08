769fae5c7402d01e23b246424dca1503f84d506e
Patrik Ondriga <xondri08@vutbr.cz>
2020-11-09 00:00:31 +0100

    Code line detection (my version)

--- src/LineHandler.java
+++ src/LineHandler.java
@@ -5,30 +5,65 @@ import org.opencv.core.Mat;
 
 public class LineHandler {
     private List<MyLine> verticalLines = new ArrayList<>();
-    private List<MyLine> horizonatLines = new ArrayList<>();
-
-    //TODO delete
-    public void test(Mat picture){
-        findLines(picture, verticalLines);
-    }
+    private List<MyLine> horizontalLines = new ArrayList<>();
 
     //TODO work only with grayscale
-    private void findLines(Mat picture, List<MyLine> lines){
-        int rows = picture.rows(); //TODO Calculates number of rows
-        int cols = picture.cols(); //TODO Calculates number of columns
+    private void findLines(Mat picture){
+        int rows = picture.rows();
+        int cols = picture.cols();
 
-        //Add first edge of the picture.
-        lines.add(new MyLine(new Coordinate(0, 0), new Coordinate(cols-1, 0)));
+        addPicturesEdges(rows, cols);
 
         for (int i=0; i<rows; i++)
         {
             for (int j=0; j<cols; j++)
             {
-                double[] data = picture.get(i, j); //TODO Stores element in an array
+                double[] data = picture.get(i, j);
+                if(data[0] == 255){
+                    int nextPixel = 0;
+                    if(i != rows-1){
+                        nextPixel = (int) picture.get(i+1, j)[0];
+                    }
+                    addLine(new Coordinate(j, i), nextPixel, this.verticalLines);
+                    if(j != cols-1){
+                        nextPixel = (int) picture.get(i, j+1)[0];
+                    }
+                    addLine(new Coordinate(j, i), nextPixel, this.horizontalLines);
+                }
             }
         }
+    }
 
-        //Add secund edge of the picture.
-        lines.add(new MyLine(new Coordinate(0, rows-1), new Coordinate(cols-1, rows-1)));
+    private void addPicturesEdges(int rows, int cols){
+        MyLine tmp = MyLine.createLine(new Coordinate(0, 0), new Coordinate(0, rows-1));
+        if(tmp != null){
+            this.verticalLines.add(tmp);
+        }
+        tmp = MyLine.createLine(new Coordinate(cols-1, 0), new Coordinate(cols-1, rows-1));
+        if(tmp != null){
+            this.verticalLines.add(tmp);
+        }
+        tmp = MyLine.createLine(new Coordinate(0, 0), new Coordinate(cols-1, 0));
+        if(tmp != null){
+            this.horizontalLines.add(tmp);
+        }
+        tmp = MyLine.createLine(new Coordinate(0, rows-1), new Coordinate(cols-1, rows-1));
+        if(tmp != null){
+            this.horizontalLines.add(tmp);
+        }
+    }
+
+    private void addLine(Coordinate coordinate, int nextPixel, List<MyLine> lines){
+        for(MyLine line : lines){
+            if(line.extendByOne(coordinate)){
+                return;
+            }
+        }
+        if(nextPixel == 255){
+            MyLine tmp = MyLine.createLine(coordinate, coordinate);
+            if(tmp != null){
+                lines.add(tmp);
+            }
+        }
     }
 }
--- src/MyLine.java
+++ src/MyLine.java
@@ -1,10 +1,25 @@
 public class MyLine {
-    private final Coordinate startPoint;
-    private final Coordinate endPoint;
+    private Coordinate startPoint;
+    private Coordinate endPoint;
 
-    public MyLine(Coordinate startPoint, Coordinate endPoint){
+    private MyLine(Coordinate startPoint, Coordinate endPoint){
         this.startPoint = startPoint;
         this.endPoint = endPoint;
+    }
+
+    public static MyLine createLine(Coordinate one, Coordinate two){
+        if(one.getX() == two.getX()){
+            if(one.getY() < two.getY()){
+                return new MyLine(one, two);
+            }
+            return new MyLine(two, one);
+        }else if(one.getY() == two.getY()){
+            if(one.getX() < two.getX()){
+                return new MyLine(one, two);
+            }
+            return new MyLine(two, one);
+        }
+        return null;
     }
 
     public Coordinate getStartPoint(){
@@ -17,9 +32,38 @@ public class MyLine {
 
     public int length(){
         if(this.startPoint.getX() == this.endPoint.getX()){
-            return Math.abs(this.startPoint.getY() - this.endPoint.getY());
+            return (this.endPoint.getY() - this.startPoint.getY() + 1);
         }else{
-            return Math.abs(this.startPoint.getX() - this.endPoint.getX());
+            return (this.endPoint.getX() - this.startPoint.getX() + 1);
         }
+    }
+
+    public boolean extendByOne(Coordinate coordinate){
+        if(this.startPoint.getX() == this.endPoint.getX()){
+            if(this.startPoint.getX() != coordinate.getX()){
+                return false;
+            }
+            if((this.startPoint.getY() - 1) == coordinate.getY()){
+                this.startPoint = coordinate;
+                return true;
+            }
+            if((this.endPoint.getY() + 1) == coordinate.getY()){
+                this.endPoint = coordinate;
+                return true;
+            }
+        }else{
+            if(this.startPoint.getY() != coordinate.getY()){
+                return false;
+            }
+            if((this.startPoint.getX() - 1) == coordinate.getX()){
+                this.startPoint = coordinate;
+                return true;
+            }
+            if((this.endPoint.getX() + 1) == coordinate.getX()){
+                this.endPoint = coordinate;
+                return true;
+            }
+        }
+        return false;
     }
 }
